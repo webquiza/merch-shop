e0b288b8d979e1f5704a1c3607992c2da7ac1365
Carlos Urquiza <webquiza@gmail.com>
2021-12-22 20:52:21 -0800

    practice using css object with styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,17 @@
 import React from "react";
 import ticketsImage from "./../img/reading_tickets.jpg";
 
-function Header(){
+// CSS object with inline styles
+function Header(props){
+  const Header = {
+    color: 'green'
+  }
   return (
     <React.Fragment>
+      <div style={Header}>
       <h1>Help Queue</h1>
       <img src={ticketsImage} alt="An image of tickets" />
+      </div>
     </React.Fragment>
   );
 }
