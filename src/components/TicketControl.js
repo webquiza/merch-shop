1e353ba9cd8e5114e1ae90bb78bd5c27d2b7a30f
Carlos Urquiza <webquiza@gmail.com>
2021-12-22 20:39:28 -0800

    update working practice project

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -7,8 +7,17 @@ class TicketControl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      formVisibleOnPage: false
+      formVisibleOnPage: false,
+      mainTicketList: []
     };
+  }
+
+  handleAddingNewTicketToList = (newTicket) => {
+    const newMainTicketList = this.state.mainTicketList.concat(newTicket);
+    this.setState({
+      mainTicketList: newMainTicketList,
+      formVisibleOnPage: false 
+    });
   }
 
   handleClick = () => {
@@ -21,10 +30,10 @@ class TicketControl extends React.Component {
     let currentlyVisibleState = null;
     let buttonText = null; 
     if (this.state.formVisibleOnPage) {
-      currentlyVisibleState = <NewTicketForm />;
-      buttonText = "Return to Ticket List"; 
+      currentlyVisibleState = <NewTicketForm onNewTicketCreation={this.handleAddingNewTicketToList} />
+        buttonText = "Return to Ticket List";
     } else {
-      currentlyVisibleState = <TicketList />;
+      currentlyVisibleState = <TicketList ticketList={this.state.mainTicketList} />;
       buttonText = "Add Ticket"; 
     }
     return (
